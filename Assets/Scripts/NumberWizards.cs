b82c0f5bcef25391cd6903c9d94e16d9eb22ddff
Gunn3r1995 <smithateden@hotmail.com>
2017-12-04 21:32:46 +0000

    addind feature

--- Assets/Scripts/NumberWizards.cs
+++ Assets/Scripts/NumberWizards.cs
@@ -4,27 +4,35 @@ using UnityEngine;
 
 public class NumberWizards : MonoBehaviour {
 
+	int min = 1;
+	int max = 1000;
+	int guess = 500;
+
 	// Use this for initialization
 	void Start () {
+		max++;
 		print ("Welcome to Number Wizard!");
 		print ("Pick a number in your head, but don't tell me!");
-
-		var min = 1;
-		var max = 1000;
 
 		print ("The highest number you can pick is " + max);
 		print ("The lowest number you can pick is " + min);
 
-		print ("Is the number higher or lower than 500?");
+		print ("Is the number higher or lower than " + guess + "?");
 		print ("Up arrow for higher, down for lower, return for equal");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			print ("Up arrow key was pressed");
+			//print ("Up arrow key was pressed");
+			min = guess;
+			guess = (max + min)/2;
+			print ("Higher or lower than " + guess + "?");
 		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			print ("Down arrow key was pressed");
+			//print ("Down arrow key was pressed");
+			max = guess;
+			guess = (max + min) / 2;
+			print ("Higher or lower than " + guess + "?");
 		} else if (Input.GetKeyDown(KeyCode.Return)) {
 			print ("I won!!!");
 		}
