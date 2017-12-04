112579fbb0d740e55bc9bb42f57b89fe651d8bf8
Gunn3r1995 <smithateden@hotmail.com>
2017-12-04 21:51:57 +0000

    changing to visual studio

--- Assets/Scripts/NumberWizards.cs
+++ Assets/Scripts/NumberWizards.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class NumberWizards : MonoBehaviour {
 
-	int min = 1;
-	int max = 1000;
-	int guess = 500;
+	int min;
+	int max;
+	int guess;
 
 	// Use this for initialization
 	void Start () {
-		max++;
+		StartGame();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			//print ("Up arrow key was pressed");
+			min = guess;
+			NextGuess();
+		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			//print ("Down arrow key was pressed");
+			max = guess;
+			NextGuess();
+		} else if (Input.GetKeyDown(KeyCode.Return)) {
+			print ("I won!!!");
+			StartGame();
+		}
+	}
+
+	void StartGame () {
+		min = 1;
+		max = 1000;
+		guess = 500;
+
+		print ("=========================");
 		print ("Welcome to Number Wizard!");
 		print ("Pick a number in your head, but don't tell me!");
 
@@ -18,23 +42,14 @@ public class NumberWizards : MonoBehaviour {
 		print ("The lowest number you can pick is " + min);
 
 		print ("Is the number higher or lower than " + guess + "?");
-		print ("Up arrow for higher, down for lower, return for equal");
+		print ("Up = Higher, Down = Lower, Return = Equal");
+
+		max++;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			//print ("Up arrow key was pressed");
-			min = guess;
-			guess = (max + min)/2;
-			print ("Higher or lower than " + guess + "?");
-		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			//print ("Down arrow key was pressed");
-			max = guess;
-			guess = (max + min) / 2;
-			print ("Higher or lower than " + guess + "?");
-		} else if (Input.GetKeyDown(KeyCode.Return)) {
-			print ("I won!!!");
-		}
+
+	void NextGuess () {
+		guess = (max + min) / 2;
+		print ("Higher or lower than " + guess + "?");
+		print ("Up = Higher, Down = Lower, Return = Equal");
 	}
 }
